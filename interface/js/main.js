492854f611682274b0b4f2ff7d7afabbd1cdf82e
CRY-TeX <blacklion.x0@gmail.com>
2020-11-16 12:00:17 +0100

    learning tracker website 1. draft

---
+++ interface/js/main.js
@@ -0,0 +1,23 @@
+
+let btn_add_session = document.getElementById("btn-add-session");
+
+let btn_start = document.getElementById("btn-start");
+let btn_stop = document.getElementById("btn-stop");
+let btn_reset = document.getElementById("btn-reset");
+
+
+btn_add_session.addEventListener("click", function () {
+    alert("add session");
+})
+
+btn_start.addEventListener("click", function () {
+    alert("start session");
+})
+
+btn_stop.addEventListener("click", function () {
+    alert("stop session");
+})
+
+btn_reset.addEventListener("click", function () {
+    alert("reset session");
+})
