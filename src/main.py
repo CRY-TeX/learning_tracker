1af94292df4dae2c252e836855cc132eaf7e3a81
CRY-TeX <blacklion.x0@gmail.com>
2020-11-21 15:25:26 +0100

    restucture project to thinker approach. Server approach may come later

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import time
+
+import tkinter as tk
+
+def main():
+    window = tk.Tk()
+    lab = tk.Label(text="Hello I'm testing")
+    lab.pack()
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
